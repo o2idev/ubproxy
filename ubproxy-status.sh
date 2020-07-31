96471e192bdf985ee4ea9a85c0c0dda25724d5e6
o2idev <68382740+o2idev@users.noreply.github.com>
2020-07-31 11:23:59 +0200

    Update ubproxy-status.sh

--- ubproxy-status.sh
+++ ubproxy-status.sh
@@ -1,13 +1,14 @@
 set -x
 echo -e "\n"
-cat /etc/bash.bashrc
+update-alternatives --query java | grep 'Value: ' | sed 's/Value: \(.*\)\/bin\/java/\1\/lib\/net.properties/' | xargs cat | egrep -i "prox"
 echo -e "\n"
-cat /etc/environment
+cat /etc/bash.bashrc | egrep -i "prox"
 echo -e "\n"
-cat /etc/apt/apt.conf
+cat /etc/environment | egrep -i "prox"
 echo -e "\n"
-set | egrep "prox|PROX"
+cat /etc/apt/apt.conf | egrep -i "prox"
+echo -e "\n"
+set | egrep -i "prox"
 echo -e "\n"
 gsettings list-recursively org.gnome.system.proxy
 echo -e "\n"
-
